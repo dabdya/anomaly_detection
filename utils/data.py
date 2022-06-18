7f7cb9e9d8efed9319b0621fc916d8942e87d1c0
Sergey Pishchulov <57267495+SergeyPishchulov@users.noreply.github.com>
2022-06-18 16:48:32 +0500

    add load_df_by_names

--- utils/data.py
+++ utils/data.py
@@ -73,3 +73,15 @@ def load_df_with_names(path, anomaly=""):
     anomaly_free_df = pd.read_csv([file for file in all_files if 'anomaly-free' in file][0], 
                                   sep=';', index_col='datetime', parse_dates=True)
     return list_of_df, anomaly_free_df, file_names
+
+
+
+
+def load_df_by_names(path, anomalies):
+    res = {}
+    
+    for anomaly in anomalies:
+        list_of_df, _, file_names = load_df_with_names(path, anomaly)
+        for df, name in zip(list_of_df, file_names):
+            res[name] = df
+    return res
